525f2ec82da8483135e61b618e6f44f97523d702
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-08-14 22:12:37 -0400

    we got a successful api call with my user data!! component is working and displaying

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,7 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-console.log(axios.get('https://api.github.com/users/aaronspurgeon'));
+// console.log(axios.get('https://api.github.com/users/aaronspurgeon'));
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -14,6 +14,11 @@ console.log(axios.get('https://api.github.com/users/aaronspurgeon'));
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+const container = document.querySelector('.cards');
+axios.get('https://api.github.com/users/aaronspurgeon')
+  .then((resp) => {
+    container.appendChild(githubCard(resp));
+  });
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
